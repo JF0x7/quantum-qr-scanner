c84f6297fc13158b0cb36a4f90b56d9f135f61a4
Jeremi Zawisza Folta <jeremifolta99@gmail.com>
2026-01-19 23:28:11 -0700

    Update scanner.js

--- scanner.js
+++ scanner.js
@@ -1,1 +1,10 @@
+// Deep link to Qtum wallet
+function sendQtum(address) {
+  window.location.href = `qtum:${address}`;
+}
 
+// Placeholder generator (we can upgrade this to real keypair generation)
+function generateQtumAddress() {
+  const fake = "Q" + Math.random().toString(36).substring(2, 34);
+  return fake;
+}
